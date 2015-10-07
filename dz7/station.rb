6b2c8459f4914e0883e9eba6702c143e5e063fcc
Ekaterina Andrusevich <jena_gj@mail.ru>
2015-10-07 22:08:44 +0300

    станции

---
+++ dz7/station.rb
@@ -0,0 +1,17 @@
+class Station
+	def initialize(name_st)
+		@name_st = name_st
+		@list_trains = []
+	end
+
+	def train_more(number)
+		@list_trains << number
+	end
+    
+    def train_less(number)
+		@list_trains.delete(number)
+	end
+
+	def list_of_trains_print
+		puts "список поездов #{@list_trains}"
+	end
